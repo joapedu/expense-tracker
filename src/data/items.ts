aa94e103d7429fc91cc726c235150588ebf5e83b
joapedu <joaoeduardobraga2@gmail.com>
2023-10-30 15:22:30 -0300

    :sparkles: feat: new itens and categories data

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    { date: new Date(2023, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2023, 9, 15), category: 'food', title: 'Burguer King', value: 28 },
+    { date: new Date(2023, 9, 16), category: 'rent', title: 'Aluguel', value: 2300 },
+    { date: new Date(2023, 10, 5), category: 'salary', title: 'Salário', value: 3600 },
+];
