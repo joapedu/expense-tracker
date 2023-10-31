0ce27378e756e247adca33dbac3c7645e867303c
joapedu <joaoeduardobraga2@gmail.com>
2023-10-31 11:09:55 -0300

    :sparkles: feat: add new data and helpers

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -4,4 +4,5 @@ export const categories: Category = {
     food: { title: 'Alimentação', color: 'blue', expense: true },
     rent: { title: 'Aluguel', color: 'brown', expense: true },
     salary: { title: 'Salário', color: 'green', expense: false },
+    others: { title: 'Outros', color: 'gray', expense: true },
 }
